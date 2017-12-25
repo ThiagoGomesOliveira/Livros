fbd39f29b420a670acf75b2ed804c2c5c77681c5
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2017-12-25 20:22:23 -0200

    ThiagoGomesOliveira
    exercicio troca de valores e resto da divisao

---
+++ WindowsForms/Capítulo01/SolucaoCap01/RestoDaDivisao/FormRestoDeDivisao.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace RestoDaDivisao
+{
+    partial class FormRestoDeDivisao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDividendo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResto = new System.Windows.Forms.TextBox();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dividendo :";
+            // 
+            // txtDividendo
+            // 
+            this.txtDividendo.Location = new System.Drawing.Point(69, 12);
+            this.txtDividendo.Name = "txtDividendo";
+            this.txtDividendo.Size = new System.Drawing.Size(53, 20);
+            this.txtDividendo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Divisor :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resto:";
+            // 
+            // txtResto
+            // 
+            this.txtResto.Enabled = false;
+            this.txtResto.Location = new System.Drawing.Point(178, 41);
+            this.txtResto.Name = "txtResto";
+            this.txtResto.Size = new System.Drawing.Size(78, 20);
+            this.txtResto.TabIndex = 4;
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.Location = new System.Drawing.Point(179, 13);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(57, 20);
+            this.txtDivisor.TabIndex = 5;
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(6, 39);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(123, 23);
+            this.btnDivisao.TabIndex = 7;
+            this.btnDivisao.Text = "Realizar Divisão";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // FormRestoDeDivisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 80);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.txtResto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDividendo);
+            this.Controls.Add(this.label1);
+            this.Name = "FormRestoDeDivisao";
+            this.Text = "Resto Da Divisao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDividendo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResto;
+        private System.Windows.Forms.TextBox txtDivisor;
+        private System.Windows.Forms.Button btnDivisao;
+    }
+}
---
+++ WindowsForms/Capítulo01/SolucaoCap01/RestoDaDivisao/FormRestoDeDivisao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestoDaDivisao
+{
+    public partial class FormRestoDeDivisao : Form
+    {
+        public FormRestoDeDivisao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            int dividendo = Convert.ToInt32(txtDividendo.Text);
+            int divisor = Convert.ToInt32(txtDivisor.Text);
+            double resto = dividendo % divisor;
+            txtResto.Text = resto.ToString();
+        }
+    }
+}
