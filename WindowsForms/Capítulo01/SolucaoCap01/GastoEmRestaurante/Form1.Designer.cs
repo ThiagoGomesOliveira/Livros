b6a7b832d3ec998d05f4544de0a1ec7b25d29f69
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2017-12-26 17:04:56 -0200

    Thiago Gomes Oliveira
    Exercicio Gasto de Restaurante

---
+++ WindowsForms/Capítulo01/SolucaoCap01/GastoEmRestaurante/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GastoEmRestaurante
+{
+    partial class formGastoEmRestaurante
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtDespesa = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Despesa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(19, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total com 10%";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(183, 27);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular ";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtDespesa
+            // 
+            this.txtDespesa.Location = new System.Drawing.Point(77, 27);
+            this.txtDespesa.Name = "txtDespesa";
+            this.txtDespesa.Size = new System.Drawing.Size(80, 20);
+            this.txtDespesa.TabIndex = 3;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BackColor = System.Drawing.Color.Yellow;
+            this.txtTotal.Enabled = false;
+            this.txtTotal.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtTotal.Location = new System.Drawing.Point(102, 56);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(156, 20);
+            this.txtTotal.TabIndex = 4;
+            // 
+            // formGastoEmRestaurante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 100);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDespesa);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "formGastoEmRestaurante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gasto Restaurante";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtDespesa;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ WindowsForms/Capítulo01/SolucaoCap01/GastoEmRestaurante/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GastoEmRestaurante
+{
+    public partial class formGastoEmRestaurante : Form
+    {
+        public formGastoEmRestaurante()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double despesa = Convert.ToDouble(txtDespesa.Text);
+            double total = despesa * 1.10;
+            txtTotal.Text = total.ToString();
+            txtDespesa.Text = "";
+        }
+    }
+}
